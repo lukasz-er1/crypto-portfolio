f663501a190dde9f5cb66a307ad338d804911552
Lukasz Rutkowski <33703141+lukasz-rutkowski@users.noreply.github.com>
2021-11-29 00:40:53 +0100

    update project structure, add basic tests

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from flask import render_template
+import json
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)).rstrip("\\app"))
+from app.config import app
+from app.utils import update_prices, make_summary, update_symbols_id_list_from_coingecko
+
+
+@app.route('/', methods=['GET'])
+def main():
+    with open('portfolio.json', 'r', encoding='utf-8') as portf:
+        portfolio = json.load(portf)
+    return render_template('index.html', portfolio=portfolio)
+
+
+@app.route('/update/', methods=['GET'])
+def update():
+    portfolio = update_prices()
+    return render_template('index.html', portfolio=portfolio)
+
+
+@app.route('/summary/', methods=['GET'])
+def summary():
+    with open('portfolio.json', 'r', encoding='utf-8') as portf:
+        portfolio = json.load(portf)
+    summary = make_summary(portfolio)
+    return render_template('summary.html', portfolio=portfolio, summary=summary)
+
+
+if __name__ == "__main__":
+    update_symbols_id_list_from_coingecko()
+    # app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='192.168.0.30', port=80, debug=True)
